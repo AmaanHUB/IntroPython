cbb217a8b75a1185847e39e158f242893548e6ed
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2020-11-02 15:39:56 +0000

    take three variables for input and outputs them

---
+++ variable_task.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+name = input("What is your name? ")
+DOB = input("What is your date of birth? ")
+age = input("What is your age? ")
+
+print("Name: ", name)
+print("Date of Birth", DOB)
+print("Age: ", age)
