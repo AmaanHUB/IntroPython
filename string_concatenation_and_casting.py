acbb67f863cfb8f9eb492533c5475160e09f8547
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2020-11-02 16:58:17 +0000

    FInished string_concatenation_and_casting notes

--- string_concatenation_and_casting.py
+++ string_concatenation_and_casting.py
@@ -33,3 +33,34 @@ print("Hello " + name)
 print("Your age is " + str(age))
 print("Your date of birth is " + str(dob))
 print("Your address is " + str(address))
+
+# Declaring strings with single and double quotes
+single_quotes = 'single_quotes\'wow\''  # can make readability and coding harder than double quotes
+
+double_quotes = "double quotes wow"
+
+# String slicing - why
+# Indexing in Python starts as 0
+greetings = "Hello World!"
+print(greetings[6:])  # leaving the second value blank does to the end of the string
+
+
+white_spaces = "lot's of spaces at the end          "
+print(len(white_spaces))
+print(len(white_spaces.strip()))
+# strip() deletes all the spaces at the end of string
+
+# count() method counts the number of times any word is used in a string
+example_text = "lots of text with some text"
+
+print(example_text.count("text"))
+
+print(example_text.replace("with", "chicken"))  # replace the 'with' with 'chicken'
+
+# capitalize() makes the first letter a capital, everything else lower
+print(example_text.capitalize())
+print(example_text.lower())
+print(example_text.upper())
+
+# fstring
+print(f"{example_text} testing")
