dd2047921d7fe3b1557bcd950f6f663190597f7a
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2020-11-02 15:46:09 +0000

    Finished task

--- variable_task.py
+++ variable_task.py
@@ -6,6 +6,7 @@ name = input("What is your name? ")
 DOB = input("What is your date of birth? ")
 age = input("What is your age? ")
 
-print("Name: ", name)
-print("Date of Birth", DOB)
-print("Age: ", age)
+print(f"Name: {name}\nDate of birth: {DOB}\nAge: {age}")
+print(
+    f"Types of variables: \nName = {type(name)}\nDate of birth = {type(DOB)}\nAge = {type(age)}"
+)
